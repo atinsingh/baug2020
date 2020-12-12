2a73a788f8f3ab019a99c6bd9b2ed2e263078826
SaritGH <you@example.com>
2020-12-12 00:10:17 -0500

    code to read excel file

--- src/test/java/io/pragra/learning/framwork/data/ExcelReader.java
+++ src/test/java/io/pragra/learning/framwork/data/ExcelReader.java
@@ -1,23 +1,21 @@
 package io.pragra.learning.framwork.data;
-
+//Read excel file using apache poi
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Iterator;
-import java.util.List;
 
 public class ExcelReader {
     public static void main(String[] args) throws IOException, InvalidFormatException {
 
       //  List<String> list = Files.readAllLines(Paths.get("C:\\Users\\Sarit\\Desktop", "QATeamMemberNames.xlsx"));
       //  list.forEach(System.out::println);
-        Path path = Paths.get("C:\\Users\\Sarit\\Desktop", "QATeamMemberNames.xlsx");
+        Path newpath = Paths.get("C:\\Users\\Sarit\\Desktop", "QATeamMemberNames.xlsx");
         Workbook workbook;
-        workbook = new XSSFWorkbook(path.toFile());
+        workbook = new XSSFWorkbook(newpath.toFile());
         Sheet sheet1 = workbook.getSheet("Names");
         Iterator<Row> rowIterator = sheet1.rowIterator();
 
---
+++ tasks.md
@@ -0,0 +1,17 @@
+## Geet
+1. Add Log4J to the project 
+2. Create log4j2.xml file to create roating log files.
+
+## Simar 
+1. Update pom.xml to add apache poi, extent report
+
+## Parita
+1. list all broswers and their driver stiring like webdriver.chrome.driver
+2. all add support versions too
+
+## Harpeet
+1. Create excel file with test data to test zoom.us, contact page 
+2. create test data for testing the profile page of zoom.us
+
+## Payel :)
+1. Write a code to read excel files and save the data in List<Object[]>
